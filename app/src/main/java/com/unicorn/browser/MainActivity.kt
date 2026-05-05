5c36bc0838f3543f1116257d6cc7060ea40f7b1a
Tab browser <debbarmaritesh576@gmail.com>
2026-05-06 00:01:47 +0530

    Create app/src/main/java/com/unicorn/browser/MainActivity.kt

---
+++ app/src/main/java/com/unicorn/browser/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.unicorn.browser
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://www.google.com")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
